29792a33447d9181a9f0b5888159098e7720b8da
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2023-04-28 16:05:12 -0700

    Connect GraphQL with blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,14 @@ module.exports = {
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `blog`,
+        // Path to the directory
+        path: `${__dirname}/blog`,
+      },
+    }
   ]
 };
