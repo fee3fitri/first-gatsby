62b7a5b9a5ed9399b773f25660eff5deb4f25a87
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2023-04-27 17:42:43 -0700

    About

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
+
+// Add Gatsby Head API to add title to the HTML head
+export const Head = () => <title>About Page</title>
--- src/pages/index.js
+++ src/pages/index.js
@@ -126,6 +126,8 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
       <h1 style={headingStyles}>
         Congratulations
         <br />
