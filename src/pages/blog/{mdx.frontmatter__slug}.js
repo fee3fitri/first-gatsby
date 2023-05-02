7033f2d9383cd05955a863c250463ea0ac2cb918
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2023-05-02 15:49:46 -0700

    Add blog graphql query

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
+import { graphql } from 'gatsby'
 
 const BlogPost = () => {
   return (
@@ -9,6 +10,18 @@ const BlogPost = () => {
     </Layout>
   )
 }
+
+export const query = graphql `
+  query ($id: String) {
+    mdx(id: { eq: $id } ) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+    }
+  }
+`
+
 
 export const Head = () => <Seo title="Super Cool Blog Posts" />
 
